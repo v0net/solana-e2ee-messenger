afda14134a6146bea2a81b70e7314b8627364695
Matt <madbenma@gmail.com>
2026-08-20 15:58:30 +0300

    feat(client): implement fetchChat and message deserialization

--- app/src/solana/solana.js
+++ app/src/solana/solana.js
@@ -89,6 +89,136 @@ function initializeChatData(peerPublicKeyString) {
   );
 }
 
+async function initializeChat() {
+  if (!userSigningKeyPair || !peerSigningPublicKey || !chatPDA || !program)
+    throw new Error("An error occurred");
+  const [participant1, participant2] =
+    Buffer.compare(
+      userSigningKeyPair.publicKey.toBuffer(),
+      peerSigningPublicKey.toBuffer(),
+    ) < 0
+      ? [userSigningKeyPair.publicKey, peerSigningPublicKey]
+      : [peerSigningPublicKey, userSigningKeyPair.publicKey];
+  await program.methods
+    .initializeChat()
+    .accounts({
+      chat: chatPDA,
+      payer: userSigningKeyPair.publicKey,
+      participant1: participant1,
+      participant2: participant2,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([userSigningKeyPair])
+    .rpc();
+}
+
+async function setEncryptionKey() {
+  if (!userSigningKeyPair || !chatPDA || !program || !userEncryptionKeyPair)
+    throw new Error("An error occurred");
+  await program.methods
+    .setEncryptionKey(userEncryptionKeyPair.publicKey)
+    .accounts({
+      chat: chatPDA,
+      sender: userSigningKeyPair.publicKey,
+    })
+    .signers([userSigningKeyPair])
+    .rpc();
+}
+
+async function sendMessage(content) {
+  if (!userSigningKeyPair || !chatPDA || !program || !chatSharedKey)
+    throw new Error("An error occurred");
+  const { encryptedContent, nonce } = encryptWithSharedKey(content);
+  await program.methods
+    .sendMessage(Buffer.from(encryptedContent), nonce)
+    .accounts({
+      chat: chatPDA,
+      sender: userSigningKeyPair.publicKey,
+    })
+    .signers([userSigningKeyPair])
+    .rpc();
+}
+
+async function closeChat() {
+  if (!userSigningKeyPair || !chatPDA || !program)
+    throw new Error("An error occurred");
+  const chatData = await program.account.chat.fetch(chatPDA);
+  await program.methods
+    .closeChat()
+    .accounts({
+      chat: chatPDA,
+      signer: userSigningKeyPair.publicKey,
+      payer: chatData.payer,
+    })
+    .signers([userSigningKeyPair])
+    .rpc();
+}
+
+async function getMessages() {
+  if (!chatPDA || !program || !chatSharedKey)
+    throw new Error("An error occurred");
+  try {
+    const chatData = await program.account.chat.fetch(chatPDA);
+    return chatData.messages.map((message) => ({
+      sender: message.sender
+        ? chatData.participant2.toBase58()
+        : chatData.participant1.toBase58(),
+      content: decryptWithSharedKey(
+        message.content,
+        new Uint8Array(message.nonce),
+      ),
+      timestamp: parseInt(message.timestamp) * 1000,
+    }));
+  } catch (error) {
+    return [];
+  }
+}
+
+async function getChats() {
+  if (!userSigningKeyPair || !program || !userEncryptionKeyPair)
+    throw new Error("An error occurred");
+  const chats = await program.account.chat.all();
+  const userChats = chats.filter(
+    ({ account }) =>
+      account.participant1.toBase58() ===
+        userSigningKeyPair.publicKey.toBase58() ||
+      account.participant2.toBase58() ===
+        userSigningKeyPair.publicKey.toBase58(),
+  );
+
+  return userChats.map(({ account }) => {
+    const peerSigningPublicKey =
+      account.participant1.toBase58() ===
+      userSigningKeyPair.publicKey.toBase58()
+        ? account.participant2.toBase58()
+        : account.participant1.toBase58();
+    if (!account.messages.length)
+      return {
+        name: peerSigningPublicKey,
+        lastMessage: null,
+        timestamp: null,
+      };
+    const peerEncryptionPublicKey =
+      account.participant1.toBase58() ===
+      userSigningKeyPair.publicKey.toBase58()
+        ? new Uint8Array(account.participant2EncryptionKey)
+        : new Uint8Array(account.participant1EncryptionKey);
+    const sharedKey = nacl.box.before(
+      peerEncryptionPublicKey,
+      userEncryptionKeyPair.secretKey,
+    );
+    return {
+      name: peerSigningPublicKey,
+      lastMessage: decryptWithSharedKey(
+        account.messages.at(-1).content,
+        new Uint8Array(account.messages.at(-1).nonce),
+        sharedKey,
+      ),
+      timestamp: parseInt(account.messages.at(-1).timestamp) * 1000,
+    };
+  });
+}
+
 
 async function checkChatExists() {
   try {
@@ -115,4 +245,7 @@ export {
   initializeData,
   initializeChatData,
   checkChatExists,
+  getMessages,
+  getChats,
+  checkEncryptionKeys,
 };
